7783f5add17b5e6a147872150d3886ed0d01298d
Hamza <mlearning51@gmail.com>
2025-02-28 17:04:42 +0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# __manifest__.py
+
+{
+    'name': 'priv Test Module new',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'summary': 'A module to demonstrate a simple test in Odoo',
+    'description': """
+        This is a test module to demonstrate how to create a simple test in Odoo.
+    """,
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [    ],
+    'installable': True,
+    'application': False,
+}
